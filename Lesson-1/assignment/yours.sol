5e122440857b4bb6e38c2b560f41fcf1f7c9e859
heyb <heyb@geohey.com>
2018-03-13 22:37:49 +0800

    何宇兵的第一课作业

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,43 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    uint constant payDuration = 10 seconds;
+    address owner;
+    uint salary = 1 ether;
+    address employee;
+    uint lastPayday = now;
+
+    function Payroll () {
+        owner = msg.sender;
+    }
+    
+    function updateEmployee(address e) {
+        require(msg.sender == owner);
+        employee = e;
+    }
+    
+    function updateSalary(uint s) {
+        require(msg.sender == owner);
+        salary = s * 1 ether;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() {
+        require(msg.sender == employee);
+        uint nextPayday = lastPayday + payDuration;
+        assert(nextPayday < now);
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+}

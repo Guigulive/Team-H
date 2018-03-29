3a6395e3c6bcbb9520e81b279006bae67e598886
Ted <ted@lifang.com>
2018-03-29 20:23:38 +0800

    add lesson 6 code

---
+++ Lesson-6/assignment/truffle.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+     development: {
+         host: "127.0.0.1",
+         port: 8545,
+         network_id: "*", // Match any network id
+         //from: "0xdC75EB0973F96b735087B6B2f20ef73595509354"
+         }
+     }
+};
